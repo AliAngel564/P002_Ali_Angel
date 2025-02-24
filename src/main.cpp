94f01ad0fa6831367e9c47e810354a02b4aa1642
AliAngel564 <jonathan.aliangel@gmail.com>
2025-02-24 09:18:17 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/*
+Amerike University
+Author: Ali Angel
+Work #: 2
+Date: 24/02/2025
+Description: This program will print various data from a specific student (me)
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    cout << "Name: Jonathan Ali Morales Angel\n"; 
+    cout << "Age: 21y/o\n";
+    cout << "Occupation: Student\n";
+    cout << "City of Origin: Guadalajara\n";
+    cout << "Favorite Hobby: Listening to music and playing videogames\n";
+    cout << "Favorite food: Lonches banados\n";
+    cout << "Music Reccomendation: Russian jazz fusion band SOYUZ, more specifically their album titled II";
+
+    return 0;
+}
